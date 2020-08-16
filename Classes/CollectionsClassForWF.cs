58f645654f0d08bd938188abdc7b938abbb3c9d1
Slava456 <67310601+Slava456@users.noreply.github.com>
2020-08-16 16:03:04 +0400

    Add GC methods to transform method

--- Classes/CollectionsClassForWF.cs
+++ Classes/CollectionsClassForWF.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows.Forms;
 using System.IO;
+using System;
 
 namespace ZipCutTransformFiles.Classes
 {
@@ -16,24 +17,36 @@ namespace ZipCutTransformFiles.Classes
             {
                 ccorign.bfclst[i].TransformAndSaveBF();
                 lbl.Text = "Выполняются преобразования файла: " + ccorign.bfclst[i].GiveYourName();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
                 pb.Value++;
             }
             for (int i = 0; i < ccorign.pclst.Count; i++)
             {
                 ccorign.pclst[i].TransformAndSavePic();
                 lbl.Text = "Выполняются преобразования файла: " + ccorign.pclst[i].GiveYourName();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
                 pb.Value++;
             }
             for (int i = 0; i < ccorign.txtlst.Count; i++)
             {
                 ccorign.txtlst[i].TransformAndSaveTXT();
                 lbl.Text = "Выполняются преобразования файла: " + ccorign.txtlst[i].GiveYourName();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
                 pb.Value++;
             }
             for (int i = 0; i < ccorign.zfclst.Count; i++)
             {
                 ccorign.zfclst[i].SaveZipFile();
                 lbl.Text = "Выполняются преобразования файла: " + ccorign.zfclst[i].GiveYourName();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
                 pb.Value++;
             }
         }
